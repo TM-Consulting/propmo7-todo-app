1a53d7ba386b72f8f0df20d9a6a83740e39d6f3f
TM-Consulting <mounaim.touinssi@gmail.com>
2021-12-13 22:06:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Tea from './components/Tea'
+import './app.css'
+const App = () => {
+  return (
+    <article>
+      <Tea/>
+      <Tea/>
+      <Tea/>
+      <Tea/>
+      <Tea/>
+      <Tea/>
+    </article>
+  );
+}
+
+export default App;
---
+++ src/components/Tea/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+const Tea =()=>
+        <div class="img-tile">
+            <img src="https://i.pinimg.com/favicons/82f8b567a43aa625429e831016bbd441632d0f02797c98975e35feb9.png?935a5f70405d1be9b3a95278ece15d6d" alt=""/>
+            <h3>The vert</h3>
+        </div>
+    
+
+
+export default Tea;
